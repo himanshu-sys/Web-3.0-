3c8b1b6ad23ea48a141d4836815cd044f23614be
Himanshu <Himanshu.developer01@gmail.com>
2022-06-28 19:26:40 +0530

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 // Import Assets
-import profile from '../assets/profile.png';
+import profile from '../assets/profile.JPG';
 
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            <img src={profile} alt="Himanshu" />
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
-                <p>Blockchain Developer</p>
+                <h1>Hi, I'm Himanshu Singh</h1>
+                <p>Website/Software Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
         </section>
